1448e2c420945a5564675ef7555d5c36a461ac9f
Timothy Nunn <54078499+timnunn@users.noreply.github.com>
2021-11-25 18:01:55 -0500

    Add files via upload

---
+++ Tim'sSportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Ninject;
+
+namespace TimsSportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            // put bindings here
+        }
+    }
+}
